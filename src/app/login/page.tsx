5231de45ef1d05081d78ca878f989a52afb060df
Michele <hello@michelemanna.me>
2024-10-24 19:47:28 +0200

    Finished login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { auth, signIn } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import { FaGithub } from "react-icons/fa";
 
 export default async function Page() {
   const session = await auth();
@@ -8,13 +9,24 @@ export default async function Page() {
   if (session?.user) return redirect("/");
 
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signIn("github");
-      }}
-    >
-      <Button type="submit">GitHub</Button>
-    </form>
+    <div className="text-center flex flex-col gap-3 h-screen justify-center items-center">
+      <h1>Login</h1>
+      <p className="muted">
+        Please click the button below to start using the drive app.
+      </p>
+
+      <form
+        onSubmit={async () => {
+          "use server";
+
+          await signIn("github");
+        }}
+      >
+        <Button variant="outline" type="submit">
+          <FaGithub />
+          GitHub
+        </Button>
+      </form>
+    </div>
   );
 }
